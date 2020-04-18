027b1209e27ca0048adc1d228851b25b4d9bdb60
Siddalingaprasad Raju <sid.rsp@gmail.com>
2020-04-18 23:31:10 +0530

    added the movie-info-service

---
+++ src/test/java/com/sidrsp/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidrsp.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
