01e72f70614095d1cfa6b74fd7b1f7732011bdd1
Siddalingaprasad Raju <sid.rsp@gmail.com>
2020-04-19 20:22:34 +0530

    changes for call other external services

--- src/main/java/com/sidrsp/movieinfoservice/resources/MovieInfo.java
+++ src/main/java/com/sidrsp/movieinfoservice/resources/MovieInfo.java
@@ -1,13 +1,23 @@
 package com.sidrsp.movieinfoservice.resources;
 
 import com.sidrsp.movieinfoservice.resources.models.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieInfo {
+
+    /* to read a app property value*/
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable String movieId){
